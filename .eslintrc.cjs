68dd1f968f18023716ef5fdd6491cba3261fa4d1
Franziska Bath <franziska.bath@strato.de>
2024-09-12 15:55:08 +0200

    update eslint and prettier formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,15 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  env: {
+    node: true
   }
 }
