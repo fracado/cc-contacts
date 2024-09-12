296ec2df348c1920652fdbe480b78e2b80e44f7a
Franziska Bath <franziska.bath@strato.de>
2024-09-12 23:43:12 +0200

    add route to contacts list

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ListView from '@/views/ListView.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: []
+	routes: [
+		{
+			path: '/',
+			name: 'contacts',
+			component: ListView,
+			meta: {
+				title: 'Contacts'
+			}
+		}
+	]
 })
 
 export default router
