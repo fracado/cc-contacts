832aa056c5927bb17cfbc8e5daa145a0955f20a9
Franziska Bath <franziska.bath@strato.de>
2024-09-12 22:13:32 +0200

    add fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCaretUp, faCaretDown, faSort } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCaretUp, faCaretDown, faSort)
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.mount('#app')
